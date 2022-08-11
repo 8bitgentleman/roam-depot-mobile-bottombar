c82043793e1aebe3cb035e5527d59ab3b303feee
Matt Vogel <mainmoniker@googlemail.com>
2022-08-10 21:43:14 -0400

    feat: add smartblock button option

--- extension.js
+++ extension.js
@@ -8,6 +8,12 @@ const panelConfig = {
        description: "Adds a button to toggle the selected block open/close",
        action:      {type:     "switch",
                      onChange: (evt) => { console.log("Switch!", evt['target']['checked']); }}},
+      {id:     "smartblock-workflow",
+      name:   "Input test",
+      description: "Adds a button to toggle a smartblock workflow. If nothing is entered a button will not be added",
+      action: {type:        "input",
+              placeholder: "none",
+              onChange:    (evt) => { console.log("Input Changed!", evt['target'['value']]); }}},
       {id:     "button-order",
        name:   "Start Index",
        description: "Where in the bottom bar to start adding the buttons",
@@ -29,6 +35,21 @@ const createIconButton = (icon) => {
 
   return popoverButton;
 };
+
+const createImageButton = (imageURL) => {
+  const popoverButton = document.createElement("button");
+  popoverButton.className = "bp3-button bp3-minimal rm-mobile-button dont-unfocus-block";
+  popoverButton.tabIndex = 0;
+
+  const popoverImage = document.createElement("img");
+  popoverImage.src = imageURL;
+  popoverImage.alt = "";
+  popoverImage.width = "15"
+
+  popoverButton.appendChild(popoverImage);
+
+  return popoverButton;
+}
 
 function toggleBlockClose() {    
   let currentBlockOpen = window.roamAlphaAPI.data.pull("[:block/open]", [":block/uid", roamAlphaAPI.ui.getFocusedBlock()['block-uid']])[':block/open'];
@@ -53,7 +74,7 @@ function addBlockCloseButton(){
   var checkForButton = document.getElementById(`${nameToUse}-flex-space`);
   if (!checkForButton) {
       var mainButton = createIconButton(iconName);
-
+      mainButton.id = nameToUse;
       var mobileBottomBar = document.getElementById("rm-mobile-bar");
       var nextIconButton = mobileBottomBar.children[2];
 
@@ -62,6 +83,41 @@ function addBlockCloseButton(){
       mainButton.addEventListener("click", toggleBlockClose);
   }
 
+}
+
+function runSmartblockWorkflow(){
+  // trigger the workflow
+  let workflow = 'test'
+  try {
+    window.roamjs.extension.smartblocks.triggerSmartblock({
+      srcName: workflow,
+      // targetName: 'October 6th, 2021'
+      targetUid: roamAlphaAPI.ui.getFocusedBlock()['block-uid']
+    });
+  } catch (error) {
+    console.error(error);
+    // expected output: ReferenceError: nonExistentFunction is not defined
+    // Note - error messages will vary depending on browser
+    alert("Smartblock Workflow does not exist")
+  }
+
+}
+
+function addSmartBlockButton(workflow) {
+  var iconURL = 'https://raw.githubusercontent.com/dvargas92495/roamjs-smartblocks/main/src/img/lego3blocks.png';
+  var nameToUse = 'bottomSmartblockButton';
+
+  var checkForButton = document.getElementById(`${nameToUse}-flex-space`);
+  if (!checkForButton) {
+      var mainButton = createImageButton(iconURL);
+      mainButton.id = nameToUse
+      var mobileBottomBar = document.getElementById("rm-mobile-bar");
+      var nextIconButton = mobileBottomBar.children[2];
+
+      nextIconButton.insertAdjacentElement("afterend", mainButton);
+
+      mainButton.addEventListener("click", runSmartblockWorkflow);
+  }
 
 }
 
@@ -88,6 +144,9 @@ async function onload({extensionAPI}) {
           if( key == 'open-close' && value==true){
             addBlockCloseButton()
           }
+          if (key == 'smartblock-workflow' && value) {
+            addSmartBlockButton(value)
+          }
         }
       }
     }, 500);  
@@ -97,11 +156,17 @@ async function onload({extensionAPI}) {
 }
 
 function onunload() {
-  const forward = document.querySelectorAll('#roam-navigation-controls_forward');
+  let openClose = document.querySelectorAll('#bottomToggleBlockClose');
 
-  forward.forEach(tog => {
+  openClose.forEach(tog => {
       tog.remove();
   });
+  let smartblockButton = document.querySelectorAll('#bottomSmartblockButton');
+
+  smartblockButton.forEach(tog => {
+      tog.remove();
+  });
+
   console.log("unload Mobile BottomBar Butto plugin");
 }
 
